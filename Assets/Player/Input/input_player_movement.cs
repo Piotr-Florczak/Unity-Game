cb2d7afe39f3986078b5e79151d4c95b8bd7ea5b
kertoip2005 <81766931+kertoip2005@users.noreply.github.com>
2023-04-28 16:07:09 +0200

    przebudowa animacji

--- Assets/Player/Input/input_player_movement.cs
+++ Assets/Player/Input/input_player_movement.cs
@@ -6,18 +6,22 @@ using UnityEngine.InputSystem;
 
 public class input_player_movement : MonoBehaviour
 {
+
+    player_animator_controller animation_controller;
     
     bool is_attack;
     bool next_attack;
     private bool isOnCooldown = false;
-    private bool isGrounded;
+    bool isGrounded;
+    public bool isAttack1 = true;
 
     public float checkRadius = 0.1f;
 
     Rigidbody2D fizyka;
     Vector2 dane_wejscowe;
-    Animator animacja;
     PlayerInput player;
+    Animator animacja;
+    
 
     [SerializeField] float przyspiesznie = 6f;
     [SerializeField] float moc_skoku = 5f;
@@ -32,24 +36,22 @@ public class input_player_movement : MonoBehaviour
     public LayerMask enemyLayers;
     public LayerMask groundLayer;
 
-
-    AnimatorClipInfo[] animatorinfo;
-    string current_animation;
-
     public int maxHealth = 100;
     public int currentHealth;
 
     public Health_bar healthBar;
 
+    string behaviour;
 
     void Start()
     {
+        animation_controller = GetComponent<player_animator_controller>();
+        animation_controller.test();
         fizyka = GetComponent<Rigidbody2D>();
         transformacja = GetComponent<Transform>();
         animacja = GetComponent<Animator>();
 
         currentHealth = maxHealth;
-
         healthBar.SetMaxHealth(maxHealth);
     }
 
@@ -59,22 +61,29 @@ public class input_player_movement : MonoBehaviour
         healthBar.SetHeath(currentHealth);
         movement();
         flip();
-        animatorinfo = this.animacja.GetCurrentAnimatorClipInfo(0);
-        current_animation = animatorinfo[0].clip.name;
-        {
-            //Debug.Log(current_animation); 
-        }
-        if (animacja.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && current_animation == "attack_2")
-        {
-            animacja.SetBool("attack(1)", false);
-        }
-        if (animacja.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && current_animation == "attack_2")
-        {
-            animacja.SetBool("attack(2)", false);
-            next_attack = false;
+        animation_controller.controller(dane_wejscowe,isGrounded,is_attack);
+    }
 
+    // movement -----------------------------
+
+    void movement()
+    {
+        fizyka.velocity = new Vector2(dane_wejscowe.x * przyspiesznie, fizyka.velocity.y);
+       
+    }
+    void flip()
+    {
+        if (dane_wejscowe.x < 0)
+        {
+            transformacja.localScale = new Vector2(-4.5f, transformacja.localScale.y);
+        }
+        if (dane_wejscowe.x > 0)
+        {
+            transformacja.localScale = new Vector2(4.5f, transformacja.localScale.y);
         }
     }
+
+    // input movement -----------------------------
 
     void OnMove(InputValue value)             
     {
@@ -89,7 +98,10 @@ public class input_player_movement : MonoBehaviour
     }
     void OnAttack(InputValue value)
     {
-        PerformAttack();
+        if (!isOnCooldown)
+        {  
+            StartCoroutine(AttackCooldown());
+        }
         is_attack = true;
 
     }
@@ -98,18 +110,7 @@ public class input_player_movement : MonoBehaviour
         is_attack = false;
     }
 
-    public void PerformAttack()
-    {
-        if (!isOnCooldown)
-        {  
-            animatinon_controler("attack");
-        
-            animatinon_controler("attack_OnMove");
-
-            
-            StartCoroutine(AttackCooldown());
-        }
-    }
+    //attack logic -------------------------------
 
     private IEnumerator AttackCooldown()
     {
@@ -121,99 +122,6 @@ public class input_player_movement : MonoBehaviour
 
         isOnCooldown = false;
     }
-
-    void movement()
-    {
-        fizyka.velocity = new Vector2(dane_wejscowe.x * przyspiesznie, fizyka.velocity.y);
-        if(Mathf.Abs(dane_wejscowe.x) > 0)
-        {
-            animatinon_controler("biega");
-            if(is_attack)
-            {
-                animatinon_controler("attack");
-            }
-        }
-        if(fizyka.velocity.y > 1)
-        {
-            animatinon_controler("skacze");
-        }
-        if(fizyka.velocity.y < -1)
-        {
-            animatinon_controler("spada");
-        }
-        if((fizyka.velocity.y == 0 && fizyka.velocity.x == 0))
-        {
-            animatinon_controler("nic");
-        }
-    }
-
-    void animatinon_controler(string behaviour)
-    {
-        if(behaviour == "attack")
-        {       
-                animacja.SetBool("attack(1)", true);
-                animacja.SetBool("Fallen", false);
-                animacja.SetBool("Jump", false);
-                animacja.SetBool("Running", false);
-
-                if(next_attack)
-                {
-                    animacja.SetBool("attack(2)", true);
-                    animacja.SetBool("Fallen", false);
-                    animacja.SetBool("Jump", false);
-                    animacja.SetBool("Running", false);
-
-                }
-                next_attack = true;
-
-        }                
-        if (behaviour == "biega") //biega
-        {
-            animacja.SetBool("Running", true);
-            animacja.SetBool("Fallen", false);
-            animacja.SetBool("attack(2)",false);
-            animacja.SetBool("attack(1)",false);
-
-        }
-        if (behaviour=="skacze") // skacze
-        {
-            animacja.SetBool("Jump", true);
-
-            animacja.SetBool("Running", false);
-            animacja.SetBool("Fallen", false);
-            animacja.SetBool("attack(1)", false);
-
-        }
-        if (behaviour=="spada") // spada
-        {
-            animacja.SetBool("Fallen", true);
-
-            animacja.SetBool("Running", false);
-            animacja.SetBool("Jump", false);
-            animacja.SetBool("attack(1)", false);
-        }
-
-        if (behaviour == "nic") //nic nie robi
-        {
-            animacja.SetBool("Fallen", false);
-            animacja.SetBool("Jump", false);
-            animacja.SetBool("Running", false);
-            animacja.SetBool("attack(2)", false);
-        }
-    }
-
-    void flip()
-    {
-        if (dane_wejscowe.x < 0)
-        {
-            transformacja.localScale = new Vector2(-4.5f, transformacja.localScale.y);
-        }
-        if (dane_wejscowe.x > 0)
-        {
-            transformacja.localScale = new Vector2(4.5f, transformacja.localScale.y);
-        }
-    }
-
     void attack_checker()
     {
         Collider2D[] hitEnemis = Physics2D.OverlapCircleAll(attack_colider.position, attackRange, enemyLayers); //do tablicy przypisywane sa wszystkie obiekty ktore znajduja sie w zdefiniowanym opszarze
@@ -234,6 +142,7 @@ public class input_player_movement : MonoBehaviour
         Gizmos.DrawWireSphere(attack_colider.position, attackRange);
     }
 
+    // others ---------------------------------
 
     public void test()
     {
---
+++ Assets/Player/Input/player_animator_controller.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_animator_controller : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody2D rb;
+    Vector2 dane_wejscowe;
+    AnimatorClipInfo[] animatorinfo;
+
+    string current_animation;
+    [SerializeField] private bool isAttack1 = false;
+    private bool isOnCooldown = true;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        animatorinfo = this.anim.GetCurrentAnimatorClipInfo(0);
+        current_animation = animatorinfo[0].clip.name;
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && current_animation == "attack_1")
+        {
+            anim.SetTrigger("IDLE");
+            isAttack1 = !isAttack1;
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && current_animation == "attack_2")
+        {
+            anim.SetTrigger("IDLE");
+            isAttack1 = !isAttack1;
+
+        }
+    }
+    public void controller(Vector2 dane_wejscowe, bool isGrounded,bool is_attack)
+    {
+        Debug.Log(isOnCooldown);
+        
+
+        if(is_attack && isOnCooldown)
+        {
+            if (isAttack1)
+            {
+                anim.SetTrigger("Attack1Trigger");
+                StartCoroutine(AttackCooldown());
+            }
+            else
+            {   
+                anim.SetTrigger("Attack2Trigger");
+                StartCoroutine(AttackCooldown());
+            }
+        }
+
+        if (Mathf.Abs(dane_wejscowe.x ) > 0 && isGrounded && isOnCooldown)
+        {
+            anim.SetTrigger("Running");
+        }
+        if(rb.velocity.y > 1 )
+        {
+            anim.SetTrigger("Jump");
+        }
+        if(rb.velocity.y < -1 && !isGrounded )
+        {
+            anim.SetTrigger("Fall");
+
+        }
+        if((rb.velocity.y == 0 && rb.velocity.x == 0) && isGrounded && isOnCooldown)
+        {
+            anim.SetTrigger("IDLE");
+        }
+    }
+
+    private IEnumerator AttackCooldown()
+    {
+        isOnCooldown = false;
+        if (current_animation == "attack_1" || current_animation == "attack_2")
+        {
+            yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length+ 0.09f);
+        }
+        isOnCooldown = true;
+    }
+
+    public void test()
+    {
+        Debug.Log("test");
+    }
+}
